e5e06c0529069b52c318f75fbcc43be638ff4e3a
phoneixsinhala-bit <phoneixsinhala@gmail.com>
2026-09-07 12:57:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ function createBot() {
     host: 'Japanese_vilage.aternos.me',
     port: 20604,
     username: 'AFK_Bot_247',
+    auth: 'offline',
     version: '1.20.6'
   });
 
-  bot.on('spawn', () => console.log('Bot successfully connected to Japanese Village!'));
-
-  bot.on('end', () => {
-    console.log('Bot disconnected. Reconnecting in 10 seconds...');
-    setTimeout(createBot, 10000);
+  bot.on('spawn', () => {
+    console.log('Bot successfully connected to Japanese Village!');
   });
 
-  bot.on('error', err => console.log(err));
+  bot.on('kicked', console.log);
+  bot.on('error', console.log);
+
+  bot.on('end', () => {
+    console.log('Bot disconnected. Retrying in 15 seconds...');
+    setTimeout(createBot, 15000);
+  });
 }
 
 createBot();
