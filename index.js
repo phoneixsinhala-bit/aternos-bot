ded44035452875da82b649f78b997431b1802916
phoneixsinhala-bit <phoneixsinhala@gmail.com>
2026-09-07 13:18:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,36 @@ app.get('/', (req, res) => res.send('Bot Status: Active'));
 app.listen(process.env.PORT || 3000, () => console.log('Server is running'));
 
 function createBot() {
-  const bot = mineflayer.createBot({
-    host: 'leech.aternos.host',
-    port: 20604,
-    username: 'AFK_Bot_247',
-    auth: 'offline',
-    version: '1.20.6'
-  });
+  // Aternos Bot Prevention Bypass Setup
+  setTimeout(() => {
+    const bot = mineflayer.createBot({
+      host: 'leech.aternos.host',
+      port: 20604,
+      username: 'AFK_Bot_247',
+      auth: 'offline',
+      version: false, // Automatically negotiate version with server
+      checkTimeoutInterval: 60000
+    });
 
-  bot.on('spawn', () => {
-    console.log('Bot successfully connected to Japanese Village!');
-  });
+    bot.on('spawn', () => {
+      console.log('Bot successfully connected to Japanese Village!');
+      // Move slightly to prevent AFK kick
+      setInterval(() => {
+        if (bot && bot.entity) {
+          bot.setControlState('jump', true);
+          setTimeout(() => bot.setControlState('jump', false), 500);
+        }
+      }, 30000);
+    });
 
-  bot.on('kicked', console.log);
-  bot.on('error', console.log);
+    bot.on('kicked', (reason) => console.log('Kicked reason:', reason));
+    bot.on('error', (err) => console.log('Bot error:', err));
 
-  bot.on('end', () => {
-    console.log('Bot disconnected. Retrying in 15 seconds...');
-    setTimeout(createBot, 15000);
-  });
+    bot.on('end', () => {
+      console.log('Bot disconnected. Retrying in 20 seconds...');
+      setTimeout(createBot, 20000);
+    });
+  }, 5000);
 }
 
 createBot();
